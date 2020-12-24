9fbdd4d59c666a6b0d2b4418b53d3496d67fc9c4
140317 <dhanesh.kumar.ust.com>
2020-12-24 18:30:48 +0530

    Login Feature added

---
+++ BDDTraining.Feature.Test/Features/Login.feature
@@ -0,0 +1,13 @@
+﻿Feature: Login funtionality
+	
+
+@Login_Function
+Scenario Outline: Authenticate user and login to the home page
+	Given the correct username as '<username>'
+	And the correct password is '<password>'
+	When I click on the submit button
+	Then I will be redirected to the Home page
+
+	Examples:
+		| username | password |
+		| 140317     | 1234   |
---
+++ BDDTraining.Feature.Test/StepDefinitions/LoginSteps.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace BDDTraining.Feature.Test.StepDefinitions
+{
+    [Binding]
+    public class LoginSteps
+    {
+        private readonly ScenarioContext _context;
+        private IWebDriver _webDriver;
+
+        public LoginSteps(ScenarioContext context)
+        {
+            _context = context;
+            _webDriver = new ChromeDriver(@"C:\Users\ustdn18\Downloads\chromedriver_win32");
+
+
+        }
+        [Given(@"the correct username as '(.*)'")]
+        public void GivenTheCorrectUsernameAs(string username)
+        {
+            _webDriver.Navigate().GoToUrl("https://github.com/login?return_to=%2F140317");
+            System.Threading.Thread.Sleep(4000);
+            IWebElement usernameField = _webDriver.FindElement(By.Name("login"));
+            usernameField.SendKeys(username);
+        }
+
+        [Given(@"the correct password is '(.*)'")]
+        public void GivenTheCorrectPasswordIs(string pwd)
+        {
+            IWebElement pwdField = _webDriver.FindElement(By.Name("password"));
+            pwdField.SendKeys(pwd);
+        }
+
+        [When(@"I click on the submit button")]
+        public void WhenIClickOnTheSubmitButton()
+        {
+
+            IWebElement btn = _webDriver.FindElement(By.Name("commit"));
+            btn.Click();
+        }
+
+        [Then(@"I will be redirected to the Home page")]
+        public void ThenIWillBeRedirectedToTheHomePage()
+        {
+
+        }
+
+    }
+}
