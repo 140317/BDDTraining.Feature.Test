f54f6c4cffe4204e97b5b0ff2c8cb85664a5c74d
140317 <dhanesh.kumar.ust.com>
2020-12-24 13:35:41 +0530

    Add project files.

---
+++ BDDTraining.Feature.Test/Features/Calculator.feature
@@ -0,0 +1,23 @@
+﻿Feature: Calculator
+	Two numbers to be added and produce the correct result
+
+@Add_TwoNumbers_Function
+Scenario: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+@Add_TwoNumbers_Iterations_Function
+Scenario Outline: Add two numbers having iterations
+	Given the first number is <number1>
+	And the second number is <number2>
+	When the two numbers are added
+	Then the result should be <sum>
+
+	Examples:
+		| number1 | number2 | sum |
+		| 20      | 30      | 50  |
+		| 12      | 6       | 18  |
+		| 45      | 25      | 70  |
+		| 20      | 40      | 60  |
---
+++ BDDTraining.Feature.Test/StepDefinitions/CalculatorSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace BDDTraining.Feature.Test.StepDefinitions
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private int num1;
+        private int num2;
+        private int total;
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number1)
+        {
+            num1 = number1;
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number2)
+        {
+            num2 = number2;
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //Action/Function
+            total = num1 + num2;
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int sum)
+        {
+            Assert.Equal(sum, total);
+        }
+    }
+}
